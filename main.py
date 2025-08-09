123a26d1cba477c550ed2fe395b7c3f79eafcf88
Mukesh Soni <mukeshsoni@gmail.com>
2025-08-09 12:56:08 +0530

    Trying to add a python mcp client to connect to our add mcp server - WIP

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
+import asyncio
 import os
 from dotenv import load_dotenv
 from openai import AzureOpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
+from mcp_client import MCPClient
+
 load_dotenv()
 model_name = "gpt-5"
 
-def main():
+async def main():
     api_key = os.getenv("OPENAI_API_KEY")
     azure_endpoint = os.getenv("AZURE_ENDPOINT")
 
     if not azure_endpoint:
         raise ValueError("AZURE_ENDPOINT environment variable is required")
 
-    client = AzureOpenAI(
-        api_version="2025-01-01-preview",
-        azure_endpoint=azure_endpoint,
-        api_key=api_key
-    )
-    conversations: list[ChatCompletionMessageParam] = [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant."
-        }
-    ]
+    mcp_client = MCPClient()
+    await mcp_client.connect_to_servers()
+    # client = AzureOpenAI(
+    #     api_version="2025-01-01-preview",
+    #     azure_endpoint=azure_endpoint,
+    #     api_key=api_key
+    # )
+    # conversations: list[ChatCompletionMessageParam] = [
+    #     {
+    #         "role": "system",
+    #         "content": "You are a helpful assistant."
+    #     }
+    # ]
 
-    while True:
-        input_str = input("Query: ")
-        conversations.append({"role": "user", "content": input_str})
-        response = client.chat.completions.create(
-            messages=conversations,
-            max_completion_tokens=12000,
-            temperature=1.0,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            model=model_name
-        )
-        print(response.choices[0].message.content)
+    # while True:
+    #     input_str = input("Query: ")
+    #     conversations.append({"role": "user", "content": input_str})
+    #     response = client.chat.completions.create(
+    #         messages=conversations,
+    #         max_completion_tokens=12000,
+    #         temperature=1.0,
+    #         top_p=1.0,
+    #         frequency_penalty=0.0,
+    #         presence_penalty=0.0,
+    #         model=model_name
+    #     )
+    #     print(response.choices[0].message.content)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
---
+++ mcp_client.py
@@ -0,0 +1,33 @@
+from contextlib import AsyncExitStack
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+
+from typing import Optional
+
+class MCPClient:
+    def __init__(self):
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+
+    async def connect_to_servers(self):
+        server_script_path = "./mcp_servers.py"
+
+        command = "python"
+        server_params = StdioServerParameters(
+            command=command,
+            args=[server_script_path],
+            env=None
+        )
+
+        stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
+        self.stdio, self.write = stdio_transport
+        # What does the enter_async_context do?
+        self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
+
+        await self.session.initialize()
+
+        # List available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("Connected to server with tools: ", [tool.name for tool in tools])
