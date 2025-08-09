a850f06da613f85d0aa7d427efbb97de4385b15f
Mukesh Soni <mukeshsoni@gmail.com>
2025-08-09 12:50:04 +0530

    Convert the single interaction with openai endpoint to a terminal chat loop where user can build on earlier conversation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from openai import AzureOpenAI
+from openai.types.chat import ChatCompletionMessageParam
 
 load_dotenv()
 model_name = "gpt-5"
@@ -17,19 +18,26 @@ def main():
         azure_endpoint=azure_endpoint,
         api_key=api_key
     )
-    response = client.chat.completions.create(
-        messages=[
-            { "role": "system", "content": "You are a helpful assistant."},
-            { "role": "user", "content": "What is 2+2?"}
-        ],
-        max_completion_tokens=12000,
-        temperature=1.0,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        model=model_name
-    )
-    print(response.choices[0].message.content)
+    conversations: list[ChatCompletionMessageParam] = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        }
+    ]
+
+    while True:
+        input_str = input("Query: ")
+        conversations.append({"role": "user", "content": input_str})
+        response = client.chat.completions.create(
+            messages=conversations,
+            max_completion_tokens=12000,
+            temperature=1.0,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            model=model_name
+        )
+        print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
